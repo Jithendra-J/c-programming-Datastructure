bacf09f0ffd4596d4eb59862532691c9ac16c524
Jithendra-J <jithujithu9709@gmail.com>
2024-09-13 16:12:30 +0530

    Add files via upload

--- main (3).c
+++ main (3).c
@@ -1,28 +1,18 @@
-#include <stdio.h>
-int main() {
-  {
-  int a, b;
-  char oper;
-  printf("Enter the number 1: ");
-  scanf("%d", &a);
-  printf("Enter the number 2:");
-  scanf("%d", &b);
-  printf("Enter the operation ( +, -, *, /): ");
-  scanf(" %c", &oper);
-  switch(oper) {
-  case '+':
-  printf("The sum is%d\n", a + b);
-  break;
-  case '-':
-  printf("The subtraction is %d\n", a-b);
-  break;
-  case '*':
-  printf("the Multiplication is %d\n",a*b);
-  break;
-  case'/':
-  if (b != 0) {
-  printf("The divison is %d\n", a / b);
-  } else {
-  printf("Error: Division by zero is not allowed.\n");
-  }
-  }
+#include<stdio.h>
+int fact()
+{
+  printf("factorial");
+  fact();
+  return 5;
+}
+void main()
+{
+  int c=fact();
+
+}
+      
+        
+    
+
+  
+
